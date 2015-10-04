1fd46d1e6f5631fb62a40a1d07c103a495e96943
Biju Kunjummen <Biju.Kunjummen@gmail.com>
2015-10-04 10:49:00 -0700

    Load Tests

---
+++ sample-load-test/project/plugins.sbt
@@ -0,0 +1,3 @@
+addSbtPlugin("io.gatling" % "gatling-sbt" % "2.1.7")
+
+addSbtPlugin("com.typesafe.sbteclipse" % "sbteclipse-plugin" % "3.0.0")
---
+++ sample-load-test/src/test/scala/msg/BasicSimulation.scala
@@ -0,0 +1,21 @@
+package msg
+
+import io.gatling.core.Predef._
+import io.gatling.http.Predef._
+import scala.concurrent.duration._
+
+class BasicSimulation extends Simulation {
+
+  val httpConf = http.baseURL("http://localhost:8888")
+
+  val headers = Map("Accept" -> """application/json""")
+
+  val scn = scenario("Get the Aggregated Page")
+    .exec(http("request_1")
+      .get("/message")
+        .queryParam("message", "hello")
+        .queryParam("delay_by","1000")
+        .queryParam("throw_exception", "false"))
+
+  setUp(scn.inject(rampUsers(21) over (5 seconds)).protocols(httpConf))
+}
